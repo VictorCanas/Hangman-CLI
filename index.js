447d30a5de851e51951cb753335c2a3ee71acac7
Victor <victorcanasint@gmail.com>
2018-01-22 11:44:12 -0500

    logic of the game

---
+++ index.js
@@ -0,0 +1,25 @@
+//Advance JavaScript - Hangman CLI 
+
+//Workflow of the game - LOGIC 
+//Make a CLI Hangman that allows a user to guess a letter 
+
+//1. Use inquirer npm package
+//2. Create constructors for word and letter 
+//3. Word - current workd the person is trying to guess 
+//4. Letter - use for the letter in the current work: either a or _ 
+
+//Try to test with only word like Aldo and then something like Unites States
+//Think of the Uppercase logic 
+//User has 10 guess 
+//There are 4 words to guess - Toyota, Honda, Lexus, Ford
+//If no words guess to left then then end game 
+
+//Guessed a letter will have to be recursive to display again 
+//The then will have to work the function/constructors 
+
+//Things to do - Today
+//Npm init
+//Npm install inquirer
+//Upload to git 
+
+//Work in the logic 
